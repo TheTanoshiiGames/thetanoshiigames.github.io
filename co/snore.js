2ffe2d3f433d7048cb665b786f15fafa9adda045
TheTanoshiiGames <60580530+TheTanoshiiGames@users.noreply.github.com>
2022-03-25 22:05:53 -0400

    Update snore.js

--- co/snore.js
+++ co/snore.js
@@ -2,6 +2,6 @@ var img = document.createElement("img");
 img.src = "https://memegenerator.net/img/instances/72602961/snorlax-used-block-the-road-its-super-effective.jpg";
 img.width = "1600";
 img.height = "1600";
-img.style.zIndex = "10";
+img.style.display = "block";
 var block = document.getElementById("hakd");
 block.appendChild(img);
